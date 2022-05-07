e08103bf7fad42fb5a678f15d501ab14d0bacf20
nicomeza <nicomezare@gmail.com>
2022-05-06 18:09:47 -0700

    Putting codes together

---
+++ MCMC.py
@@ -0,0 +1,53 @@
+# MCMC code
+
+import numpy as np
+from distance import *
+
+def logpriors(pars):
+    omega_m = pars[0]
+    logp=-np.inf
+    logp = 0 if omega_m > 0 else logp
+    return logp    
+
+def log_likelihood(pars,z_data,m_b,cov_matrix,number_steps=100, M=-19.2):
+    
+    pars = omega_m, omega_a ,H_0
+    
+    mu_model=distance_modulus(z_data,omega_m,omega_a,H_0,number_steps)
+    m_model = mu_model+M
+
+    inv_cov=np.linalg.inv(cov_matrix)
+    
+    residual = m_b-m_model
+    result=-(np.dot( np.dot(residual, inv_cov) ,residual ))/2.
+    
+    return result
+
+def log_posterior(params,x,y,cov):
+
+    return log_likelihood(pars,x,y,cov) + logpriors(pars)
+
+
+def generating_function(omega_k, omega_lambda, omega_m, cov = 0.1 * np.identity(3)):
+    return np.random.multivariate_normal([omega_k, omega_lambda, omega_m], cov)
+
+def generating_function(Omega, cov = 0.1 * np.identity(3)):
+    return np.random.multivariate_normal(Omega, cov)
+
+
+def MCMC(seed,x,y,sigma=1):
+    '''seed is the previous value in parameter space.
+    y is the vector of observed values {y_i}.
+    x is the vector of observed predictors {x_i}
+    cov is the covariance matrix
+    sigma is the meta-parameter controlling the width of the gaussian in the generator function.'''
+    new = generator(seed,sigma)
+    Pnew = log_posterior(new,x,y,cov)
+    Pold = log_posterior(seed,x,y,cov)
+    r = Pnew-Pold
+    u = np.ln(np.random.uniform(0,1))
+    if r<u:
+        return seed
+    else:
+        return new
+
---
+++ distance.py
@@ -0,0 +1,91 @@
+# vectorized version
+import numpy as np
+from scipy.constants import c
+
+def integrand(z,omega_k,omega_m,omega_a):
+    return 1/np.sqrt(omega_m*(1+z)**3 + omega_k*(1+z)**2 + omega_a)
+    
+def trapezoidal(upper_limit,omega_m,omega_a,number_steps=10000,lower_limit=0):
+    
+    omega_k=1-omega_m-omega_a
+    
+    summ = .5*integrand(lower_limit,omega_k,omega_m,omega_a) + .5*integrand(upper_limit,omega_k,omega_m,omega_a)
+    step_size = (upper_limit-lower_limit)/number_steps
+    #k_points = arange(lower_limit,upper_limit,h)
+    #equivalent to 
+    k_points = np.linspace(lower_limit,upper_limit,int(number_steps),endpoint=False)
+
+    for _ in k_points:
+        summ += integrand(_,omega_k,omega_m,omega_a)
+    return summ*step_size
+
+def simpsons(upper_limit,omega_m,omega_a,number_steps=1000,lower_limit=0):
+    
+    number_steps=int(number_steps)
+    
+    #must have even number of steps
+    if number_steps %2!=0:
+        number_steps+=1
+        print("Number of steps modifed:",number_steps)
+        
+    omega_k=1-omega_m-omega_a
+    summ = integrand(lower_limit,omega_k,omega_m,omega_a) + integrand(upper_limit,omega_k,omega_m,omega_a)
+    step_size = (upper_limit-lower_limit)/number_steps
+    
+    #sum odd values of i
+    for i in range(1,number_steps,2):
+        summ+=4*integrand(lower_limit+i*step_size,omega_k,omega_m,omega_a)
+        
+        
+    #sum even values of i
+    for i in range(2,number_steps,2):
+        summ+=2*integrand(lower_limit+i*step_size,omega_k,omega_m,omega_a)
+        
+    
+    return summ*step_size/3
+
+def luminosity_dist(z,omega_m,omega_a,H_0,number_steps=10000,method='s'):
+    
+    omega_k=1-omega_m-omega_a
+
+    d_h = c*1E-3/H_0
+    
+    if method == "t":
+        d_c = d_h*trapezoidal(z,omega_m,omega_a,number_steps)
+    
+    #Use Simpson's method by default 
+    else:
+        d_c = d_h*simpsons(z,omega_m,omega_a,number_steps)
+    # integral value
+    #1.504993571457544*c*1E-3/H_0*(1+z)
+    
+    if omega_k>0:
+        return d_h*np.sinh(np.sqrt(omega_k)*d_c/d_h)/np.sqrt(omega_k)*(1+z)
+    
+    elif omega_k==0:
+        return d_c*(1+z)
+    
+    else:
+        return d_h*np.sin(np.sqrt(abs(omega_k))*d_c/d_h)/np.sqrt(abs(omega_k))*(1+z)
+
+#         
+
+def distance_modulus(z,omega_m,omega_a,H_0, number_steps=10000):
+    luminosity_distance = luminosity_dist(z,omega_m,omega_a,H_0,number_steps)
+    # to-do: check consistency of units
+    return 5*np.log(luminosity_distance/10) 
+
+
+def test_trapezoidal_vectorization():
+    zs = np.array([1,2,3])
+    for z, output in zip(zs, trapezoidal(zs,1,1,5)):
+        assert output == trapezoidal(z,1,1,5)
+    print('looks good')
+
+
+def test_luminosity_dist_3():
+    print(luminosity_dist(3,.286,.714,69.6,100,"s"))
+    print("Reference value:", 25924.3)
+
+test_trapezoidal_vectorization()
+test_luminosity_dist_3()
