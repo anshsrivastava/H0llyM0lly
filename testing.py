ab54a2a99e811489a7f8873e4a3d30fcbaf37d59
Ansh Srivastava <50ansh.srivastava@gmail.com>
2022-05-11 11:23:03 -0700

    Added test for simpsons

--- testing.py
+++ testing.py
@@ -1,6 +1,25 @@
 from distance import *
 from MCMC import *
 import random 
+import numpy as np
+
+def test_simpsons():
+    '''
+    Tests the simpsons() function from distance.py
+    '''
+    lower_limit=0
+    upper_limit=3
+    number_of_points=10
+    h = (upper_limit-lower_limit)/number_of_points
+    omega_m=.286
+    omega_a=.714
+    omega_k=1-omega_m-omega_a
+    k_points = np.arange(lower_limit,upper_limit+.1*h,h)
+    f_k=np.zeros(len(k_points),float)
+    for k in range(len(k_points)):
+        f_k[k]=integrand(k_points[k],omega_k,omega_m,omega_a)
+    assert (1.504993571457544-simpsons(upper_limit,omega_m,omega_a,number_of_points)) < 1E-4 # Comparing against a 10^-4 tolerance
+    print('simpsons looks good')
 
 def test_trapezoidal_vectorization():
     '''
